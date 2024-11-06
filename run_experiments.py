f2f6753855b5781bbc2ce0a625478dcc7c27e19b
Eduardo Aguilar <earguila00@hotmail.com>
2024-11-06 12:36:09 +0000

    corrected path and added function to check outliers

--- run_experiments.py
+++ run_experiments.py
@@ -1,5 +1,6 @@
 import os
 from scripts_experiments.train_GNN import train_network_nested_cv
+from utils.utils_model import aggregate_outliers 
 from options.base_options import BaseOptions
 import os
 
@@ -8,11 +9,12 @@ def run_all_exp():
     opt = BaseOptions().parse()
 
     if opt.train_GNN:
-        if not os.path.exists(os.path.join(opt.log_dir_results, opt.filename[:-4], 'results_GNN')):
+        if not os.path.exists(os.path.join(opt.log_dir_results, opt.filename[:-4], 'learning', 'results_GNN')):
             train_network_nested_cv(opt)
         else:
             print('GNN model has already been trained')
     
+    aggregate_outliers(opt)
 
 if __name__ == '__main__':
     run_all_exp()
